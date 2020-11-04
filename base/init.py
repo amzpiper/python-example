a76f7592d3a875c841bb9628d1238b7b334d44e6
guoyha <guoyha@adtec.com.cn>
2020-11-04 19:34:47 +0800

    'update'

---
+++ base/init.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+
+# # 1.hello world
+# print("hello-world")
+# print("1","2","3")
+#
+# # 2.input()
+# name = input()
+# print(name)
+#
+# name = input("please enter your name:")
+# print("hello",name,".")
+#
+
+# 数值类型
+print(10_000_000_000+0.00001)
+print("r\\\t")
+print('''
+123
+''')
+a=123
+print(not a>5 or not 5>1)
+a="123"
+print(a)
+
