8ef14fae29a09ba7be8297e930a2c8472ef780ba
guoyha <guoyha@adtec.com.cn>
2020-11-06 18:00:30 +0800

    'update-2020-11-6-高阶函数'

--- baselearn/__init__.py
+++ baselearn/__init__.py
@@ -15,7 +15,7 @@
 # print("hello",name,".")
 #
 
-# 数值类型
+# 3.数值类型
 print(10_000_000_000+0.00001)
 print("r\\\t")
 print('''
@@ -26,7 +26,7 @@ print(not a>5 or not 5>1)
 a="123"                             # 字符串
 print(a)
 
-# 编码问题,pyton3.0- 使用Unicode编码
+# 4.编码问题,pyton3.0- 使用Unicode编码
 a = ord('A')                        # ord把字符转化为整数
 print(a)
 a = chr(65)                         # chr把
@@ -35,7 +35,7 @@ x = b'ABC'                          # byte类型
 x = b'\xe4\xb8\xad\xe6\x96\x87'.decode('utf-8', errors='ignore')     # 把bytes变为str
 print(len('abc'))
 
-# 格式化输出 "%d" % (12)
+# 5.格式化输出 "%d" % (12)
 print('Hello %s,my age is %d' % ('apple', 23))          # %
 print("Hello {0},my age is {1}".format("apple", 23))    # format
 print("Hello {0},my age is {1}".format("apple", 23))    # f-string
@@ -43,7 +43,7 @@ age = 23
 name = "apple"
 print(f"Hello {age:.2f},my age is {name}")
 
-# List
+# 6.List
 classmate = ['Me','You', 'He', 'She']
 print(classmate)
 print(len(classmate))
@@ -55,14 +55,14 @@ classmate.pop()
 classmate.pop(1)
 print(classmate)
 
-# tuple
+# 7.tuple
 classmate = ('Me','You', 'He', 'She')
 class2 = (1,['Me','You', 'He', 'She'])
 class2[1][0] = 'x'
 class2[1][1] = 'y'
 print(class2)
 
-# 条件判断
+# 8.条件判断
 age = 20
 if age > 20:
     print("you are old")
@@ -77,13 +77,13 @@ elif age < 20:
 else:
     print("too old")
 
-# input()返回的数据类型是str，str不能直接和整数比较，必须先把str转换成整数。Python提供了int()函数来完成
+# 9.input()返回的数据类型是str，str不能直接和整数比较，必须先把str转换成整数。Python提供了int()函数来完成
 age = "200"
 birth = int(age)
 if birth == 200:
     print("number")
 
-# 循环 for x in array:
+# 10.循环 for x in array:
 classmate = ('Me','You', 'He', 'She')
 for mate in classmate:
     print(mate)
@@ -97,7 +97,7 @@ for x in range(101):
     sumNum += x
 print(sumNum)
 
-# while
+# 11.while
 sumNum = 0
 n = 99
 while n > 0:
@@ -105,7 +105,7 @@ while n > 0:
     n -= 2
 print(sumNum)
 
-# dict字典dictionary=map,键值对储存,具有极高的查询速度
+# 12.dict字典,dictionary=map,键值对储存,具有极高的查询速度
 classmate = {"me":100, "you":100, "she":100}
 classmate["he"] = 1000
 print(classmate["he"])
@@ -129,17 +129,19 @@ classmate.pop("he")
 # 占用空间小，浪费内存很少。
 # 所以，dict是用空间来换取时间的一种方法。
 
-# set 创建一个set，需要提供一个list作为输入集合
+# 13.set,创建一个set，需要提供一个list作为输入集合
 s = set([1,2,3,1,2,3])
 s.add(4)
 s.remove(4)
 print(s)
 
-# 交集并集
+# 13.1交集并集
 s1 = ([1,2,3])
 s2 = ([2,3,4])
+s1 & s2
+s1 | s2
 
-# 函数
+# 14.函数
 def my_abs( xx ):
     if not isinstance(x,(int,float)):
         raise TypeError('bad operand type')
@@ -150,11 +152,11 @@ def my_abs( xx ):
 
 print(my_abs(12))
 
-# 空函数
+# 14.1 空函数
 def nop():
     pass
 
-# 返回多个数值, 返回值是一个tuple
+# 14.2 返回多个数值, 返回值是一个tuple
 import math
 def move(x1, y1, step, angle=0):
     nx = x1 + step * math.cos(angle)
@@ -169,7 +171,7 @@ print(x2, y2)
 def power(x3):
     return x3 * x3
 
-# n = 2 默认参数:降低调用函数的难度
+# 14.3 默认参数,n = 2 默认参数:降低调用函数的难度
 def powerN(x4, n1 = 2):
     ss = 1
     while n1 > 0:
@@ -184,7 +186,7 @@ def add_end(L = None):
     L.append("END")
     return L
 
-# 可变参数
+# 14.4 可变参数
 # 要定义出这个函数，我们必须确定输入的参数。
 # 由于参数个数不确定，我们首先想到可以把a，b，c……作为一个list或tuple传进来，这样，函数可以定义如下：
 def calc(numbers):
@@ -205,7 +207,7 @@ def calc(*numbers):
 num1 = [1,2,3]
 calc(*num1)
 
-# 关键字参数
+# 14.5 关键字参数
 def person(name,age,**city):
     print("name:",name,"age:",age,"city:",city)
 # 可以只传入必选参数name,age,或传入任意数量的city,这些关键字参数在函数内部自动组装为一个dict
@@ -213,7 +215,7 @@ person("yuhang",12,gender="M",job = "Enginer")
 extra = {'city': 'Beijing', 'job': 'Engineer'}
 person('Jack', 24, **extra)
 
-# 命名关键字
+# 14.6 命名关键字
 # 要限制关键字参数的名字，就可以用命名关键字参数，例如，只接收city和job作为关键字参数。
 def person(name,age,*,city,job):
     print(name,age,city,job)
@@ -247,7 +249,7 @@ args = (1,2,3,4,5)
 kw = {'d':99,'x':"#"}
 farg(*args,**kw)
 
-# 递归函数
+# 14.7 递归函数
 def a(x):
     if x==1:
         return 1
@@ -272,7 +274,7 @@ def fact(num):
 print(fact(100))
 # 多数编程语言没有针对尾递归做优化，Python解释器也没有做优化，即使把上面的fact(n)函数改成尾递归方式，也会导致栈溢出.
 
-# 切片
+# 15.切片
 # 经常取指定索引范围的操作，用循环十分繁琐
 L = ["1","2","3","4","5"]
 L = list(range(101))
@@ -284,20 +286,20 @@ print(L[:])
 print(L[-10:])
 print('ABCDEFG'[:3])
 
-# 迭代
+# 15.迭代
 # 在Python中，迭代是通过for ... in来完成的，Python的for循环不仅可以用在list或tuple上，还可以作用在其他可迭代对象上。
 # 默认情况下，dict迭代的是key。如果要迭代value，可以用for value in d.values()
 # 同时迭代key和value，可以用for k, v in d.items()
 
 # 通过collections模块的Iterable类型判断，判断一个对象是可迭代对象
-from collections import Iterable
+from collections.abc import Iterable
 flag = isinstance('abc', Iterable)
 print(flag)
 # 转化为有索引下标方式进行遍历
 for i,value in enumerate(["A","B","C"]):
     print(i,value)
 
-# 列表生成式
+# 16.列表生成式
 list(range(100))
 # 生成的元素x * x放到前面，后面跟for循环，后面还可以加上if判断
 L = [x * x for x in range(1, 11) if x % 2 == 0]
@@ -305,3 +307,95 @@ print(L)
 # 还可以使用两层循环，可以生成全排列：
 L = [x + y for x in 'ABC' for y in 'XYZ']
 print(L)
+
+L = [x if x % 2 == 0 else -x for x in range(1,11)]
+print(L)
+
+# 17.生成器Generator
+# 列表元素可以按照某种算法推算出来，那我们是否可以在循环的过程中不断推算出后续的元素呢
+# 一边循环一边计算的机制，称为生成器：generator
+# 第一种方法很简单，只要把一个列表生成式的[]改成()，就创建了一个generator
+G = (x for x in range(100))
+for x in G:
+    print(x)
+
+# 17.1.斐波拉契数列:列表生成式写不出来，但是，用函数把它打印出来却很容易
+def fib(max):
+    n,a,b = 0,0,1
+    while n<max:
+        print("b:",b)
+        a,b = b, a+b
+        n = n + 1
+    return 'done'
+fib(6)
+
+# 要把fib函数变成generator，只需要把print(b)改为yield b就可以了
+# 函数定义中包含yield关键字，那么这个函数就不再是一个普通函数，而是一个generator
+def fib(max):
+    n,a,b = 0,0,1
+    while n<max:
+        yield b
+        a,b = b, a+b
+        n += 1
+    return 'done'
+fib(6)
+
+# for循环拿不到generator的return语句返回值，必须使用stopIteration错误，返回值包含在stopIterator的value中
+g = fib(6)
+while True:
+    try:
+        x = next(g)
+        print("g:",x)
+    except StopIteration as e:
+        print("Generator return value:",e.value)
+        break
+
+# 18.Iterable and Iterator
+from collections.abc import Iterable
+# isinstance 来判断对象是否是Iterable对象
+# 可以直接作用于for循环的对象统称为可迭代对象：Iterable。
+# isinstance 检测 list
+print("是否是Iterable对象")
+print("检测 list：",isinstance([],Iterable))
+# isinstance 检测 dict
+print("检测 dict：",isinstance({},Iterable))
+# isinstance 检测 tuple
+print("检测 tuple：",isinstance((1,),Iterable))
+# isinstance 检测 string
+print("检测 string：",isinstance('abc',Iterable))
+# isinstance 检测 生成器
+print("检测 生成器：",isinstance((x for x in range(101)),Iterable))
+# isinstance 检测 set
+print("检测 set：",isinstance(([]),Iterable))
+
+
+from collections.abc import Iterator
+# isinstance 来判断对象是否是Iterator对象
+# 可以被next()函数调用并不断返回下一个值的对象称为迭代器：Iterator
+# isinstance 检测 list
+print("是否是Iterable对象")
+print("检测 list：",isinstance([],Iterator))
+# isinstance 检测 dict
+print("检测 dict：",isinstance({},Iterator))
+# isinstance 检测 tuple
+print("检测 tuple：",isinstance((1,),Iterator))
+# isinstance 检测 string
+print("检测 string：",isinstance('abc',Iterator))
+# isinstance 检测 生成器
+print("检测 生成器：",isinstance((x for x in range(101)),Iterator))
+# isinstance 检测 set
+print("检测 set：",isinstance(([]),Iterator))
+
+# 生成器都是Iterator对象，但list、dict、str虽然是Iterable，却不是Iterator。
+# 把list、dict、str等Iterable变成Iterator可以使用iter()函数：
+# 为什么list、dict、str等数据类型不是Iterator？
+#
+# 这是因为Python的Iterator对象表示的是一个数据流，
+# Iterator对象可以被next()函数调用并不断返回下一个数据，
+# 直到没有数据时抛出StopIteration错误。可以把这个数据流看做是一个有序序列，
+# 但我们却不能提前知道序列的长度，只能不断通过next()函数实现按需计算下一个数据，
+# 所以Iterator的计算是惰性的，只有在需要返回下一个数据时它才会计算。
+
+
+# 19.高阶函数
+#
