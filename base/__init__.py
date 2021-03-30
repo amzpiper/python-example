db985dbcd002f4f4be03eb0108e29baa1e7e40db
郭宇航 <1131717341@qq.com>
2021-03-30 12:55:24 +0800

    test

--- base/__init__.py
+++ base/__init__.py
@@ -1188,3 +1188,51 @@ print(10 / n)
 # pdb.set_trace() # 运行到这里会自动暂停
 
 # 22.3.单元测试
+print("单元测试")
+
+import unittest
+class Dict(dict):
+    def __init__(self,**kw):
+        super().__init__(**kw)
+
+    def __getattr__(self,key):
+        try:
+            return self[key]
+        except KeyError:
+            raise AttributeError(r"'Dict' object has no attribute '%s'" % key)
+    
+    def __setattr__(self, key, value):
+        self[key] = value
+
+
+class Test(unittest.TestCase):
+
+    def setUp(self) -> None:
+        print("setUp")
+
+    def tearDown(self) -> None:
+        print("tearDown")
+
+    def test_init(self):
+        d = Dict(a=1, b='test')
+        self.assertEqual(d.a, 1)
+        self.assertEqual(d.b, 'test')
+        self.assertTrue(isinstance(d, dict))
+
+    def test_key(self):
+        d = Dict()
+        d['key'] = 'value'
+        self.assertEqual(d['key'],'value')
+
+    def test_keyerror(self):
+        d = Dict()
+        with self.assertRaises(KeyError):
+            value = d['empty']
+
+    def test_attrerror(self):
+        d = Dict()
+        with self.assertRaises(AttributeError):
+            value = d.empty
+
+if __name__ == '__main__':
+    unittest.main()
